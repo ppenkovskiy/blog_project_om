91e8ea5438a96b3f92de232bce1c7bc5d8fa1d18
p_penkovskiy <p.penkovskiy@gmail.com>
2022-02-01 00:30:53 +0300

    c3

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def posts_list(request):
-    return HttpResponse('<h1>Hello from posts_list<h1>')
+    n = ['pavel', 'masha', 'olya', 'ksu']
+    return render(request, 'blog/index.html', context={'names': n})
--- app/blogengine/blogengine/settings.py
+++ app/blogengine/blogengine/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'blogengine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+    ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
