16c6a914c05b0db635c85ee02209eb1c1f4ac597
p_penkovskiy <p.penkovskiy@gmail.com>
2022-02-01 15:02:05 +0300

    c4

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
+
 def posts_list(request):
-    n = ['pavel', 'masha', 'olya', 'ksu']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
